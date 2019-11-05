7c2600e9c67bfd1b25cf4d03607e617911d072ab
Argent <argent172172@gmail.com>
2019-11-05 21:40:33 +0200

    v0.1

--- script/script.js
+++ script/script.js
@@ -1,15 +1,15 @@
 function start() {
-    btn = document.getElementById("btn");
-    nova = document.getElementById("supernova");
-    wrapper = document.getElementById("wrapper");
-    title = document.getElementById("title");
-    nova.style.animation = "Explosion 1s linear";
+    supernova.style.animation = "Explosion 1s linear";
     btn.style.display = "none";
     wrapper.style.display = "flex";
     title.style.animation = "MoveTitle 12s 0.5s";
-    setTimeout(function() {var audio = new Audio('title1.mp3');
-	audio.play();}, 2000);
-	
-    
-
+    setTimeout(function() {
+        var audio = new Audio('title1.mp3');
+        audio.play();
+    }, 2000);
 }
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+    btn.addEventListener('click', start);
+// });
