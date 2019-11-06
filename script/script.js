39229b7a94c4dba4f4fc80ea7aff7f6288cc30a0
Argent <argent172172@gmail.com>
2019-11-06 12:04:51 +0200

    mobile

--- script/script.js
+++ script/script.js
@@ -1,12 +1,14 @@
 function start() {
     supernova.style.animation = "Explosion 1s linear";
     btn.style.display = "none";
+    setTimeout(function() {
     wrapper.style.display = "flex";
     title.style.animation = "MoveTitle 12s 0.5s";
+    }, 800);
     setTimeout(function() {
         var audio = new Audio('title1.mp3');
         audio.play();
-    }, 2000);
+    }, 000);
 }
 
 
